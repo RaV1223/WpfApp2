059cf82ae39d30c18b808e9fe923a8e8d0dc412b
Rafał Pociecha <rafal.pociecha.rp@gmail.com>
2023-05-04 14:16:26 +0200

    One corrupted line, dont know why but it should work

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,41 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Navigation;
 
 namespace WpfApp2
 {
-   
-
-    private void startButton_Click(object sender, RoutedEventArgs e)
-    {
-        AddEnemy();
-    }
- private void AddEnemy()
-    {
-        ContentControl enemy = new ContentControl();
-    }
-   
-
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     /// 
-    public sealed partial class MainPage : MainWindow.Common.LayoutAwarePage
+
+    public  sealed partial class MainWindow : Window
     {
-        Random random = new Random();
-    }
-    public partial class MainWindow : Window
-    {
+         Random random = new Random(); 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public class Common
+        private void Start_Click(object sender, RoutedEventArgs e)
         {
-            public class LayoutAwarePage
+            AddEnemy();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100),
+                random.Next((int)playArea.ActualHeight - 100), "Canvas.Top");
+            playArea.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
             {
-            }
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation,enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
         }
     }
 }
